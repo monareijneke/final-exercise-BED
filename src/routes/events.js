acca0b9f637c976a95ce23f354ec9266a2d55126
Mona Reijneke <cooljay.mr@gmail.com>
2024-01-21 14:34:49 +0100

    making routes async/await

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import e, { Router } from "express";
 import createEvent from "../services/events/createEvent.js";
 import getEventById from "../services/events/getEventById.js";
 import getEvents from "../services/events/getEvents.js";
@@ -8,85 +8,109 @@ import authMiddleware from "../middleware/auth.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  const { title } = req.query;
-  const events = getEvents(title);
-  res.json(events);
-});
-
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const event = getEventById(id);
-
-  if (event) {
-    res.status(200).json(event);
-  } else {
-    res.status(404).send({ message: `Event with id ${id} not found!` });
+router.get("/", async (req, res, next) => {
+  try {
+    const { title } = req.query;
+    const events = await getEvents(title);
+    res.json(events);
+  } catch (error) {
+    next(error);
   }
 });
 
-router.post("/", authMiddleware, (req, res) => {
-  const {
-    createdBy,
-    title,
-    description,
-    image,
-    categoryIds,
-    location,
-    startTime,
-    endTime,
-  } = req.body;
-  const newEvent = createEvent(
-    createdBy,
-    title,
-    description,
-    image,
-    categoryIds,
-    location,
-    startTime,
-    endTime
-  );
-  res.status(201).json(newEvent);
-});
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const event = await getEventById(id);
 
-router.delete("/:id", authMiddleware, (req, res) => {
-  const { id } = req.params;
-  const event = deleteEventById(id);
-
-  if (event) {
-    res.status(200).send({ message: `Event with id${id} succesfully dleted` });
-  } else {
-    res.status(404).json({ message: `Event with id${id} not found!` });
+    if (event) {
+      res.status(200).json(event);
+    } else {
+      res.status(404).send({ message: `Event with id ${id} not found!` });
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
-router.put("/:id", authMiddleware, (req, res) => {
-  const { id } = req.params;
-  const {
-    createdBy,
-    title,
-    description,
-    image,
-    categoryIds,
-    location,
-    startTime,
-    endTime,
-  } = req.body;
-  const event = updateEventById(id, {
-    createdBy,
-    title,
-    description,
-    image,
-    categoryIds,
-    location,
-    startTime,
-    endTime,
-  });
+router.post("/", authMiddleware, async (req, res, next) => {
+  try {
+    const {
+      createdBy,
+      title,
+      description,
+      image,
+      categoryIds,
+      location,
+      startTime,
+      endTime,
+    } = req.body;
+    const newEvent = await createEvent(
+      createdBy,
+      title,
+      description,
+      image,
+      categoryIds,
+      location,
+      startTime,
+      endTime
+    );
+    res.status(201).json(newEvent);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  if (event) {
-    res.status(200).send({ message: `Event with id${id} succesfully updated` });
-  } else {
-    res.status(404).json({ message: `Event with ${id} not found!` });
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const event = await deleteEventById(id);
+
+    if (event) {
+      res
+        .status(200)
+        .send({ message: `Event with id${id} succesfully dleted` });
+    } else {
+      res.status(404).json({ message: `Event with id${id} not found!` });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const {
+      createdBy,
+      title,
+      description,
+      image,
+      categoryIds,
+      location,
+      startTime,
+      endTime,
+    } = req.body;
+    const event = await updateEventById(id, {
+      createdBy,
+      title,
+      description,
+      image,
+      categoryIds,
+      location,
+      startTime,
+      endTime,
+    });
+
+    if (event) {
+      res
+        .status(200)
+        .send({ message: `Event with id${id} succesfully updated` });
+    } else {
+      res.status(404).json({ message: `Event with ${id} not found!` });
+    }
+  } catch (error) {
+    next(error);
   }
 });
 export default router;
