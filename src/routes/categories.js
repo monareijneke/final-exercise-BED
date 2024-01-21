b054300abacaa8dd583ac7b8e87a6e430670ed28
Mona Reijneke <cooljay.mr@gmail.com>
2024-01-21 11:21:22 +0100

    making routes async/await

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -8,52 +8,72 @@ import authMiddleware from "../middleware/auth.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  const categories = getCategories();
-  res.json(categories);
-});
-
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const category = getCategoryById(id);
-
-  if (category) {
-    res.status(200).json(category);
-  } else {
-    res.status(404).json({ message: `Category with id${id} not found!` });
+router.get("/", async (req, res, next) => {
+  try {
+    const categories = await getCategories();
+    res.json(categories);
+  } catch (error) {
+    next(error);
   }
 });
 
-router.delete("/:id", authMiddleware, (req, res) => {
-  const { id } = req.params;
-  const category = deleteCategoryById(id);
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const category = await getCategoryById(id);
 
-  if (category) {
-    res
-      .status(200)
-      .send({ message: `Category with id ${id} succesfully deleted` });
-  } else {
-    res.status(404).json({ message: `Category wirh id${id} not found` });
+    if (category) {
+      res.status(200).json(category);
+    } else {
+      res.status(404).json({ message: `Category with id${id} not found!` });
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
-router.post("/", authMiddleware, (req, res) => {
-  const { name } = req.body;
-  const newCategory = createCategory(name);
-  res.status(201).json(newCategory);
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const category = await deleteCategoryById(id);
+
+    if (category) {
+      res
+        .status(200)
+        .send({ message: `Category with id ${id} succesfully deleted` });
+    } else {
+      res.status(404).json({ message: `Category wirh id${id} not found` });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put("/:id", authMiddleware, (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  const category = updateCategoryById(id, { name });
+router.post("/", authMiddleware, async (req, res, next) => {
+  try {
+    const { name } = req.body;
+    const newCategory = await createCategory(name);
+    res.status(201).json(newCategory);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  if (category) {
-    res
-      .status(200)
-      .send({ message: `Category with id${id} succesfully updated` });
-  } else {
-    res.status(404).json({ message: `Category with id${id} not found` });
+router.put("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    const category = await updateCategoryById(id, { name });
+
+    if (category) {
+      res
+        .status(200)
+        .send({ message: `Category with id${id} succesfully updated` });
+    } else {
+      res.status(404).json({ message: `Category with id${id} not found` });
+    }
+  } catch (error) {
+    next(error);
   }
 });
 export default router;
