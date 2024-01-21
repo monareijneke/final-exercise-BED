f68f055032df8b0be9ca771fe49e4237563a482c
Mona Reijneke <cooljay.mr@gmail.com>
2024-01-21 10:09:19 +0100

    upload events app
    working events app (createCategory already in Prisma mode).
    Testing in terminal all green.
    Postman query all correct status returned

---
+++ src/services/events/getEventById.js
@@ -0,0 +1,7 @@
+import eventData from "../../data/events.json" assert { type: "json" };
+
+const getEventById = id => {
+  return eventData.events.find(event => event.id === id);
+};
+
+export default getEventById;
